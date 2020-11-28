060b37b4189194ea28316b1279c8fc0f6c36f55d
Mango0x45 <thomasvoss@live.com>
2020-11-28 19:50:22 +0100

    Add problem 2

---
+++ project_euler/2/even_fibonacci_numbers.asm
@@ -0,0 +1,48 @@
+%include "../macros.inc"
+
+section .bss
+    count   resq 1
+    cPos    resb 1
+
+section .text
+    global  _start
+
+; Initialize rbx and rcx
+_start:
+    mov     rbx, 1
+    mov     rcx, 1
+
+; Check if rdx % 2 == 0
+_fibonacci:
+    mov     rax, rcx
+    mov     r8, 2
+    div     r8
+    cmp     rdx, 0
+    je      _isEven
+
+; Compute the next fibonacci number
+_fibonacciLoop:
+    mov     rax, rbx
+    mov     rbx, rcx
+    lea     rcx, [rax+rbx]
+
+    cmp     rcx, 0x3D0900   ; 4,000,000
+    jl      _fibonacci
+    jmp     _exit
+
+; Add rcx to count
+_isEven:
+    mov     rax, [count]
+    add     rax, rcx
+    mov     [count], rax
+
+    jmp     _fibonacciLoop
+
+; EXIT SUCCESS
+_exit:
+    mov     rax, [count]
+    prax    count, cPos
+
+    mov     rax, 60
+    xor     edi, edi
+    syscall
